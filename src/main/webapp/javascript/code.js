b8f86f3c2f156df859c62849eb0808fbcc5ac52e
Vaibhav <vaibhavgupta_iitd@yahoo.co.in>
2014-02-03 14:39:08 +0530

    Adding code editor

--- src/main/webapp/javascript/code.js
+++ src/main/webapp/javascript/code.js
@@ -1,8 +1,19 @@
+var editor,
+logEditor = $("#log-js");
+var i = 10;
 $(function() {
-	var editor = CodeMirror.fromTextArea(document.getElementById("code-js"), {
+	editor = CodeMirror.fromTextArea(document.getElementById("code-js"), {
 		lineNumbers : true,
 		mode : "javascript",
 		gutters : [ "CodeMirror-lint-markers" ],
 		lint : true
 	});
 });
+$("#runCode").click(function(){
+	eval(editor.getValue());
+});
+
+function log(statement){
+	var evalStatement = eval(statement);
+	logEditor.append("<p>"+evalStatement+"</p>");
+}
